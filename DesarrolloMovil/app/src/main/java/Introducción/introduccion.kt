1fbc2dc43efb1a780fb7520c196fdc6becfb3529
jonecto <josen.rios@urosario.edu.co>
2024-02-20 18:20:15 -0500

    Reorganización

---
+++ DesarrolloMovil/app/src/main/java/Introducción/introduccion.kt
@@ -0,0 +1,11 @@
+package com.jonecto.desarrollomovil
+
+class introduccion{
+
+}
+
+fun main() {
+    print("holamundo \n")
+    var nombre: String = "Nico"
+    println("el nombre es $nombre \n")
+}
